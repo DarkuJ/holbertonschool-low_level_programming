58a19a9953c9d519f05357db99ff534556117d96
darkuj <darkudj@gmail.como>
2022-09-15 12:12:27 -0700

    git test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,")
-return 0
+	puts("\"Programming is like building a multilingual puzzle,");
+return (0);
 }
