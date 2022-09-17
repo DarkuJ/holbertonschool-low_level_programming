063f79b6c71ee61eee4ccb196c8af9bc31fb6a1d
darkuj <darkudj@gmail.como>
2022-09-17 11:11:16 -0700

    0 pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main- Prints a random number and states whether
- * it is positive, negative, or zero.
+//**
+ * main- Prints a random numbers even odd or zero
  *
  * Returrn: Always 0
  */
+
 int main(void)
 {
 	int n;
