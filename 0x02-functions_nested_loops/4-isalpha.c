5671c10c103ebdcdb56d4e1a0a5038661d8c0354
darkuj <darkudj@gmail.como>
2022-09-19 11:54:08 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#includ "main.h"
+/**
+ * int _isalpha
+ * Description: checks if alpha or not
+ * Return: (1) if c is a ltter, else return (0)
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
