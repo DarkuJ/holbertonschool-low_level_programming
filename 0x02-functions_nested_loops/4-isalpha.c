ff0c6387baad022d331312ccc32b07b93deedd1e
darkuj <darkudj@gmail.como>
2022-09-20 07:07:03 -0700

    4 is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
- * int _isalpha
- * Description: checks if alpha or not
- * Return: (1) if c is a ltter, else return (0)
+ * int _isalpha - program startup
+ *
+ * Description: Uses _isalpha to find upper or lowercase
+ * and returns `1` if `c` is lowercase or uppercase
+ * and returns `0` otherwise
+ * Return: return (0) exits program successfully, return (1) throws an error
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
