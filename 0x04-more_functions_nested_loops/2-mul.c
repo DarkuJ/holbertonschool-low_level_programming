1ac6d1daba430673fb8c34bef77a021b57472ec8
darkuj <darkudj@gmail.como>
2022-09-23 10:19:24 -0700

    2 multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of 2 ints
+ * @a: an int given by the main function
+ * @b: an int given by the main function
+ *
+ * Description: uses header to link and nest loops to achive goal
+ * Return: returns 2 ints
+ */
+
+int mul(int a, int b)
+{
+	rreturn (a * b);
+}
