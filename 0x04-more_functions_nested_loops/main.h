59f309a265050311979560201b75ad9305238f0d
darkuj <darkudj@gmail.como>
2022-09-23 09:42:12 -0700

    0 is upperr

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,11 @@
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void - prototype (void)
+ * int - prototype (int)
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
