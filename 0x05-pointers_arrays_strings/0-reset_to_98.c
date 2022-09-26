4a3954ffb264a3c76217d07d2028bb58fc959da2
darkuj <darkudj@gmail.como>
2022-09-26 08:23:54 -0700

    0 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - working prototype
+ *@n: pointer to n
+ * Description: points to an int as paremeter and updates value to 98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
