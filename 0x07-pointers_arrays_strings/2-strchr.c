f9846085241e9f72c5ff90a1123820b356a0d761
darkuj <darkudj@gmail.como>
2022-10-04 15:07:35 -0700

    2 strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,6 +10,8 @@
 
 char *_strchr(char *s, char c)
 {
+	int i;
+
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
@@ -18,5 +20,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return (NULL);
+	return (0);
 }
