c8431428927135605039c904e698402592622bf0
darkuj <darkudj@gmail.como>
2022-10-04 17:18:28 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - functionto print chess
+ * @a: name of first variable
+ *
+ * Return: 0 if success
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int ind1, ind2;
+
+	for (ind1 = 0; a[ind1][7]; indn1++)
+	{
+		for (ind2 = 0; ind2 < 8; ind2++)
+			_putchar(a[ind1][ind2]);
+
+		_putchar('\n');
+	}
+}
