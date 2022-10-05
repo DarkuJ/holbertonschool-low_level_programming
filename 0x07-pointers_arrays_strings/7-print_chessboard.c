08af675536bc60aad427d2be2c32ea33d3096f51
darkuj <darkudj@gmail.como>
2022-10-04 17:23:52 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 {
 	int ind1, ind2;
 
-	for (ind1 = 0; a[ind1][7]; indn1++)
+	for (ind1 = 0; a[ind1][7]; ind1++)
 	{
 		for (ind2 = 0; ind2 < 8; ind2++)
 			_putchar(a[ind1][ind2]);
