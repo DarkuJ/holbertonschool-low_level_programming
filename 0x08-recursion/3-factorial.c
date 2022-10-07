e16e9b818a9d8f8516f4f50366a68f06a5df03e9
darkuj <darkudj@gmail.como>
2022-10-07 08:58:56 -0700

    3  factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -13,5 +13,14 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	return (n * factorial (n - 1));
+	return (n * factorial(n - 1));
+}
+
+int main(void)
+{
+	int f;
+
+	f = factorial (5);
+	printf("5! = %d\n", f);
+	return (0);
 }
