dbac48be0d528309bd78338d7030fa3aa526b22f
darkuj <darkudj@gmail.como>
2022-10-07 08:32:30 -0700

    0 puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - puts recursion of a string
+ *@s: given int
+ *
+ * Return: 0 if success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == 0)
+		_putchar ('\n');
+		return 0;
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+A r ead  me for 0x08
